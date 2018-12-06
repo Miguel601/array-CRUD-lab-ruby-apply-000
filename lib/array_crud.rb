9eca730c7b698635908356686c7e80911c949cde
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-12-06 00:09:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Scott", "Jeff", "Martha", "Julio"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Scott", "Jeff", "Martha", "Julio"] + "Joe"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  "Anthony" + ["Scott", "Jeff", "Martha", "Julio"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["Scott", "Jeff", "Martha", "Julio"]
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["Scott", "Jeff", "Martha", "Julio"].shift
 end
 
 def retrieve_element_from_index(array, index_number)
